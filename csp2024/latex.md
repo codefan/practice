5582a87a2e083cf1d7807e01ee0985627d256196
codefan <codefan@centit.com>
2025-01-10 16:25:56 +0800

    练习

---
+++ csp2024/latex.md
@@ -0,0 +1,8 @@
+$$
+\begin{document}
+begin{itemize}
+  item
+  item
+end{itemize}
+end{document}
+$$
--- csp2024/testCase.cpp
+++ csp2024/testCase.cpp
@@ -1,24 +1,41 @@
 #include <iostream> // 标准输入输出流 cin、cout 所在的库
 #include <cstdio>  // 标准输入输出文件 stdin、stdout和相关函数fgets、fputs所在的文件
 #include <cstring> 
+#include <cmath> 
+#define ML 100000
 using namespace std;
-int main(){
-   
-   int num[1000];
-   int l = 0;
-   int n;
-   cin >> n;
-   while(n !=1){
-      num[l++] = n;
-      if(n%2==0)
-         n >>= 1;
-      else 
-         n = n*3+1;
-   }
-   cout <<1;
-   for(int i=l-1; i>=0; i--)
-      cout <<' '<<num[i];
-   cout << endl;
 
-   return 0;
+int sq(int k){
+   return sqrt(k) + 0.05;
+}
+
+int main(){
+   int n;
+	cin>>n;
+	int s = sq(n);
+	if(s * s==n){
+		cout << 1 << endl;
+		return 0;
+	}
+	for(int i=1;i<=s;i++)
+	{
+		int s1 = sq(n - i*i);
+		if(s1 * s1 ==n-i*i){
+			cout << 2 << endl;
+			return 0;
+		}
+	}
+	for(int i=1; i<=s; i++)
+	{
+		for(int j=i; j<=s; j++)
+		{
+			int s1=sq(n - i*i - j*j);
+			if(s1*s1 == n - i*i - j*j){
+				cout << 3 << endl;
+				return 0;
+			}
+		}
+	}
+	cout << 4 << endl;
+	return 0;
 }
